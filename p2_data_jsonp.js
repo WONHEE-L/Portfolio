b802552f9a0b93a2efe7d21522e0f702990ee4c2
wonheelee <whee2013@gmail.com>
2022-08-19 18:26:21 -0400

    add Json file

---
+++ p2_data_jsonp.js
@@ -0,0 +1,22 @@
+showItems({
+    "items": [
+        {
+            "title": "Special Menu",            
+            "photo": "imageP2/i_special.png",
+            "description": "8OZ GRILLED SIRLOIN: Serve with seasonal fresh vegetables, roasted potatoes, choice of dressing $45.50 | BBQ BACK RIBS: Pork ribs with bbq sauce, serve with dill avocado, choice of customer order $45.50",
+            "button": "VIEW MORE MENU"
+        },
+        {
+            "title": "Brunch Menu",            
+            "photo": "imageP2/i_brunch.png",
+            "description": "FRENCH TOAST: Cover with fresh egg wash, toast in butter, topping with cinnamon, serving with fresh fruits bowl $25.00 | HOUSE HAMBURGER: Servve with fresh baked burger bun, topping with coleslaw, smoked brisket, sliced pickle, mustard sauce $25.00",
+            "button": "VIEW MORE MENU"
+        },
+        {
+            "title": "Dinner Menu",          
+            "photo": "imageP2/i_dinner.png",
+            "description": "GRILLED NEWYORK STEAK: Serve with fresh salad, choice of side dishes, and cook of customer order $50.50 | GRILLED SALMON STEAK: Serve with roated asparagus, choice of side dishes, and cook of customer order $50.50",
+            "button": "VIEW MORE MENU"
+        },        
+    ]
+});
